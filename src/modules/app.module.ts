02cf95e97c91760a42310ec28ccb0b1a721ae0ab
zeann3th <longhienly112@gmail.com>
2025-03-15 15:20:07 +0700

    feat: health-check route

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,10 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { SectionModule } from './section/section.module';
 import { FloorModule } from './floor/floor.module';
 import { SlotModule } from './slot/slot.module';
+import { HealthcheckModule } from './healthcheck/healthcheck.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    HealthcheckModule,
     AuthModule,
     SectionModule,
     FloorModule,
---
+++ src/modules/healthcheck/healthcheck.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { HealthcheckService } from './healthcheck.service';
+
+@ApiTags("Healthcheck")
+@Controller('healthz')
+export class HealthcheckController {
+
+  constructor(private readonly healthcheckService: HealthcheckService) { }
+
+  @ApiOperation({ summary: "Health Check", description: "Check if the server is running" })
+  @ApiResponse({ status: 200, description: "Server is running" })
+  @Get()
+  healthCheck() {
+    return this.healthcheckService.healthCheck();
+  }
+}
